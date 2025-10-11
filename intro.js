1308a8f9a0053fa981c070d891564b12f8bc6bd3
Karugia22 <mercywanjiku5968@gmail.com>
2025-10-11 15:42:27 -0700

    initial commit: Introduction Project

---
+++ intro.js
@@ -0,0 +1,8 @@
+let name = "Mercy Karugia";
+let age = 22;
+let favoriteLanguage = "C++";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
+
